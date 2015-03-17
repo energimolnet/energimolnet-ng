b6eb8ceb8267abdb8eeb84a9cd5c431aa5da3df0
Torsten Freyhall <torsten@freyhall.se>
2015-03-17 09:45:02 +0100

    Changed contracts query path

--- src/models/contracts.js
+++ src/models/contracts.js
@@ -2,7 +2,7 @@ module.exports = function(ngModule) {
   ngModule.factory('emContracts', [
     'emResourceFactory',
     function(resourceFactory) {
-      return resourceFactory({default: '/contracts', query: '/accounts/me/contracts'}, ['get', 'query', 'save']);
+      return resourceFactory({default: '/contracts'}, ['get', 'query', 'save']);
     }
   ]);
 };
