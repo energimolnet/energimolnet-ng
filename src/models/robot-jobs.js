e487d898330f568dfa1ee55172d147e0c3405bc1
Torsten Freyhall <torsten@freyhall.se>
2015-04-08 14:22:51 +0200

    Updated robot jobs path

--- src/models/robot-jobs.js
+++ src/models/robot-jobs.js
@@ -2,7 +2,7 @@ module.exports = function(ngModule) {
   ngModule.factory('emRobotJobs', [
     'emResourceFactory',
     function(resourceFactory) {
-      return resourceFactory({default: '/robotjobs'}, ['get', 'query', 'save', 'delete']);
+      return resourceFactory({default: '/robot_jobs'}, ['get', 'query', 'save', 'delete']);
     }
   ]);
 };
